7a11fc51ab404994d5bb41b2757faf59fbc0a7c7
Sergeyt2000 <sergeyt2000@gmail.com>
2025-01-15 15:51:37 +0200

    04 js done

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,29 @@
 'use strict';
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    pricePerDroid * quantity
-  } credits!`;
+function isEnoughCapacity(products, containerSize) {
+  let fruitSum = 0;
+  for (let i = 0; i < Object.values(products).length; i++) {
+    fruitSum += Object.values(products)[i];
+  }
+  // if (fruitSum <= containerSize) {
+  //   return true;
+  // } else {
+  //   return false;
+  //  }
+  return fruitSum <= containerSize ? true : false;
+  
 }
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
